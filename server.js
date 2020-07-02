7d333579319d7a79223c61a21227d91f29141809
Glitch (hello-express) <none>
2020-07-02 06:17:06 +0000

    🎥🎟 Checkpoint ./views/index.pug:18003481/197 ./server.js:18003481/403

--- server.js
+++ server.js
@@ -20,12 +20,17 @@ db.defaults({
     { title: "GOD Father", description: "Lorem ipsum dolor sit amet" }
   ]
 }).write();
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.get("/books", function(req, res) {
   res.render("index", { databooks: db.get("databooks").value() });
 });
-
+app.post("/books", function(req, res) {
+  console.log(req.body);
+  // res.redirect('/books');
+});
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
