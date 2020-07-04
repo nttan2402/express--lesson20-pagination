f9acdbd64fb9b1a234471d8d696e6c6dd9b650af
Glitch (hello-express) <none>
2020-07-04 15:03:15 +0000

    👹👘 Checkpoint ./users.route.js:18003481/2296 ./views/users.pug:18003481/12 ./views/index.pug:18003481/20 ./db.js:18003481/115

--- db.js
+++ db.js
@@ -6,9 +6,9 @@ var db = low(adapter);
 
 db.defaults({
   databooks: [
-    { title: "GOD Father", description: "Lorem ipsum dolor sit amet" },
-    { title: "GOD Father", description: "Lorem ipsum dolor sit amet" },
-    { title: "GOD Father", description: "Lorem ipsum dolor sit amet" }
+    { title: "GOD Father", description: "Lorem ipsum dolor sit amet", id: "abcsdos129n" },
+    { title: "GOD Father", description: "Lorem ipsum dolor sit amet", id: "abcsq2os19n"  },
+    { title: "GOD Father", description: "Lorem ipsum dolor sit amet", id: "abcsdosvb9n"  }
   ],
   users: [{ name: "Tan", age: 21 }, { name: "Nam", age: 21 }]
 }).write();
--- users.route.js
+++ users.route.js
@@ -1,32 +1,26 @@
-var express = require('express')
-var router = express.Router()
-var db = require('./db');
-var shortid = require('shortid')
+var express = require("express");
+var router = express.Router();
+var db = require("./db");
+var shortid = require("shortid");
 
 router.get("/", function(req, res) {
   res.render("users", { users: db.get("users").value() });
 });
 router.get("/update/:name", function(req, res) {
-  var name = req.params.name;
-  res.render("update", { name: name });
+  res.render("update", { name: req.params.name });
 });
 router.post("/update/:name", function(req, res) {
-  var oldname = req.params.name;
-  var newname = req.body.name; //{name: abc}
-  var newage = req.body.age; //{age: abc}
-  req.body.id = shortid.generate()
+  req.body.id = shortid.generate();
   db.get("users")
-    .find({ name: oldname })
-    .assign({ name: newname, age: newage })
+    .find({ name: req.params.name })
+    .assign(req.body)
     .write();
-
   res.redirect("/users");
 });
 
 router.post("/delete/:name", function(req, res) {
-  var oldname = req.params.name;
   db.get("users")
-    .remove({ name: oldname })
+    .remove({ name: req.params.name })
     .write();
   res.redirect("/users");
 });
@@ -35,10 +29,10 @@ router.get("/create", function(req, res) {
   res.render("create");
 });
 router.post("/create", function(req, res) {
-  req.body.id = shortid.generate()  
+  req.body.id = shortid.generate();
   db.get("users")
     .push(req.body)
     .write();
   res.redirect("/users");
 });
-module.exports = router
+module.exports = router;
