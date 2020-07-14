df7e59e07bf9e005487ddd27834ae2ee6be45bbf
Glitch (hello-express) <none>
2020-07-14 15:44:01 +0000

    🐈🎂 Checkpoint ./views/create.pug:18003481/13 ./controllers/users.controller.js:18003481/10

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -28,10 +28,8 @@ module.exports.create = function(req, res) {
 }
 module.exports.postCreate = function(req, res) {
   var errors = [];
-    
   if(req.body.name.length >= 31) {
     errors.push('the Length of the name is greater 30 characters');
-    
     res.render("create", {
       errors: errors
     });
