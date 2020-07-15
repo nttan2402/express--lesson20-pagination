a5485e33158098d57f75378bf1e70fa9230f1502
Glitch (hello-express) <none>
2020-07-15 09:09:33 +0000

    🍜🎰 Checkpoint ./validation/users.validation.js:18003481/256

--- validation/users.validation.js
+++ validation/users.validation.js
@@ -9,11 +9,19 @@ module.exports.postCreate = function (req, res, next) {
     errors.push('Age is required')
   }
   if(errors.length) {
-    res.render("./users/create", {
+    res.render("create", {
       errors: errors,
       values: req.body
     })
     return;
   }
+  if(req.body.name.length >= 31) {
+    errors.push('the Length of the name is greater 30 characters');
+    res.render("create", {
+      errors: errors
+    });
+    return;
+  }
   next();
 }
+//
