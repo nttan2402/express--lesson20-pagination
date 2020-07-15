249b23519b4adcd61c570d5add27734db24d3b2c
Glitch (hello-express) <none>
2020-07-15 08:46:35 +0000

    🚗🐵 Checkpoint ./db.json:18003481/93 ./layout/common.pug:18003481/1275 ./views/home.pug:18003481/243 ./views/index.pug:18003481/2755 ./views/users.pug:18003481/911 ./validation/users.validation.js:18003481/135

--- validation/users.validation.js
+++ validation/users.validation.js
@@ -1,5 +1,14 @@
 module.exports.postCreate = function (req, res, next) {
     var errors = [];
+  
+  if(!req.body.name) {
+    errors.push('Name is required')
+  }
+
+  if(!req.body.age) {
+    errors.push('Age is required')
+  }
+  
   if(req.body.name.length >= 31) {
     errors.push('the Length of the name is greater 30 characters');
     res.render("create", {
