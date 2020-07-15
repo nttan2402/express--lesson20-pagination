53df8da86ef675a128ff32f858220491bebd6210
Glitch (hello-express) <none>
2020-07-15 14:40:18 +0000

    📼🚐 Checkpoint ./server.js:18003481/331 ./package.json:18003481/1429

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+1
 
 
 // server.js
@@ -8,19 +8,28 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 var express = require("express");
 var db = require("./db");
+var shortid = require("shortid");
 var booksRoute = require("./Route/books.route");
 var usersRoute = require("./Route/users.route");
 var transactionRoute = require("./Route/transactions.route");
+var cookieParser = require('cookie-parser');
 // our default array of dreams
-
+var count = 0;
 var app = express();
+app.use(cookieParser());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.get("/", function(req, res){
-  res.render("home")
+	res.cookie('users-id', shortid.generate());
+  	res.render("home");
+  console.log(req.cookies);
+  	if(req.cookies) {
+  		count++;
+  	}
+  	console.log('cookes:', count);
 })
 //Books
 app.use("/books", booksRoute);
